ea8eb778105e999360fcff1c4bc26033a0a17d63
David W. Hogg <dwhogg@gmail.com>
2012-10-11 13:30:52 -0400

    plot title

--- mofa/test.py
+++ mofa/test.py
@@ -7,7 +7,7 @@ from mofa import *
 np.random.seed(13)
 
 
-d = np.random.randn(10,250)
+d = np.random.randn(10, 250)
 d[0,:] *= 5
 d[1,:] *= 1
 d[1,:] += 3 + 0.5 * d[0,:]
@@ -28,13 +28,13 @@ b[1,:] += 12 + 0.5 * b[0,:]
 
 d = np.concatenate((d,b),axis=1)
 
-
+D, N = d.shape
 fig=pl.figure()
 pl.plot(d[0,:],d[1,:],'ko',alpha=0.25)
 
-k,m = 2,3
+K, M = 3, 2
 
-mix = Mofa(d.T,k,m,False)
+mix = Mofa(d.T, K, M, False)
 pl.plot(mix.means[:,0],mix.means[:,1],'rx',ms=15,label='Initialization')
 mix.plot_2d_ellipses(0,1,edgecolor='r')
 
@@ -43,13 +43,13 @@ mix.run_em()
 mix.plot_2d_ellipses(0,1,edgecolor='b')
 pl.plot(mix.means[:,0],mix.means[:,1],'bx',ms=15,label='Psi free')
 
-mix = Mofa(d.T,k,m,True)
+mix = Mofa(d.T, K, M, True)
 
 mix.run_em()
 
 mix.plot_2d_ellipses(0,1,edgecolor='g')
 pl.plot(mix.means[:,0],mix.means[:,1],'gx',ms=15,label='Psi fixed')
-pl.title('Data (K,D) = ({0},{1}), Model (K,M) = ({2},{3})'.format(4,5,k,m))
+pl.title(r'Data $(D, N) = ({0}, {1})$, Model $(K, M) = ({2}, {3})$'.format(D,N,K,M))
 pl.legend()
 fig.savefig('mofa_ex.png')
 
