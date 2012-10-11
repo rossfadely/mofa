cbad4080ab406d204e2b5169373f9518e9c49a7c
Ross Fadely <rossfadely@gmail.com>
2012-10-11 13:23:18 -0400

    some test code

--- mofa/test.py
+++ mofa/test.py
@@ -6,10 +6,27 @@ from mofa import *
 
 np.random.seed(13)
 
-d = np.random.randn(5,250)
-d[0,:] *= 3
-d[1,:] *= 2
-d[1,:] += -3 + 0.5 * d[0,:]
+
+d = np.random.randn(10,250)
+d[0,:] *= 5
+d[1,:] *= 1
+d[1,:] += 3 + 0.5 * d[0,:]
+
+b = np.random.randn(10,250)
+b[0,:] *= 5
+b[0,:] -= 5 
+b[1,:] *= 1
+b[1,:] += 10 - 0.5 * b[0,:]
+
+d = np.concatenate((d,b),axis=1)
+
+b = np.random.randn(10,130)
+b[0,:] *= 1
+b[0,:] += 5
+b[1,:] *= 5
+b[1,:] += 12 + 0.5 * b[0,:]
+
+d = np.concatenate((d,b),axis=1)
 
 
 fig=pl.figure()
@@ -36,27 +53,3 @@ pl.title('Data (K,D) = ({0},{1}), Model (K,M) = ({2},{3})'.format(4,5,k,m))
 pl.legend()
 fig.savefig('mofa_ex.png')
 
-"""
-
-d = np.random.randn(10,250)
-d[0,:] *= 5
-d[1,:] *= 1
-d[1,:] += 3 + 0.5 * d[0,:]
-
-b = np.random.randn(10,250)
-b[0,:] *= 5
-b[0,:] -= 5 
-b[1,:] *= 1
-b[1,:] += 10 - 0.5 * b[0,:]
-
-d = np.concatenate((d,b),axis=1)
-
-b = np.random.randn(10,130)
-b[0,:] *= 1
-b[0,:] += 5
-b[1,:] *= 5
-b[1,:] += 12 + 0.5 * b[0,:]
-
-d = np.concatenate((d,b),axis=1)
-
-"""
